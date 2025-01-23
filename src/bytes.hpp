8dff94269c418378f1cc983e73a495944864420b
John R. Patek <johnpatek2@gmail.com>
2025-01-23 16:23:30 -0600

    save point

--- src/bytes.hpp
+++ src/bytes.hpp
@@ -6,6 +6,28 @@
 #include <algorithm>
 #include <string>
 
+/*
+ * Copyright 2025 Maxtek Consulting
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 namespace wadtek
 {
     std::function<void(uint8_t*,size_t)> get_swap_func()
@@ -16,6 +38,7 @@ namespace wadtek
         if (test_ptr[0] != 0x02)
         {
             swap_func = [](uint8_t *data, size_t size) {
+                // possible bug here, I got a segfault when running on LE
                 std::reverse(data, data + size);
             };
         }
